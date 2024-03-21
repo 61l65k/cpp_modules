3325b192c2d80869a46c93a66fcee62d86891b90
alex <alexxpyykonen@gmail.com>
2024-03-21 16:28:49 +0200

    ex00 ready

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
-
-static char ft_toupper(char c)
-{
-    if (c >= 'a' && c <= 'z')
-        return (c - 'a' + 'A');
-    return (c);
-}
+#define NO_ARGS_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 int main(int argc, char **argv) {
     if (argc < 2)
+        std::cout << NO_ARGS_MSG;
+    for (int i = 1; i < argc; i++)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return (1);
-    }
-    for (int i = 1; i < argc; ++i)
-    {
-        for (int j = 0; argv[i][j]; ++j)
-            std::cout << ft_toupper(argv[i][j]);
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)std::toupper(argv[i][j]);
     }
     std::cout << std::endl;
     return (0);
