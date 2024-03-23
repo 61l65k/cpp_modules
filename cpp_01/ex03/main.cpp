9efb3772590894de774fc12bdf547e3126cbf0e3
alex <alexxpyykonen@gmail.com>
2024-03-23 04:11:56 +0200

    Add informative coment

--- cpp_01/ex03/main.cpp
+++ cpp_01/ex03/main.cpp
@@ -6,12 +6,22 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 03:43:51 by apyykone          #+#    #+#             */
-/*   Updated: 2024/03/23 04:01:05 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/03/23 04:11:41 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 #include "HumanB.hpp"
+
+/*
+
+For HumanA (Bob), we use a reference to Weapon because Bob must always have a weapon, and the weapon type remains constant throughout Bob's lifetime.
+
+For HumanB (Jim), we use a pointer to Weapon because Jim may not always have a weapon initially, and he can equip different weapons during his lifetime.
+
+So said the reference is always same as the pointer can dynamically change.
+
+*/
 
 int main()
 {
