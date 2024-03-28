321698bfea64cb6e51da75947e276806e85eba48
alex <alexxpyykonen@gmail.com>
2024-03-28 12:32:17 +0200

    + Add the ex02 fully + create more informative comment for ex01

--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -6,32 +6,26 @@
 /*   By: apyykone <apyykone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 09:37:50 by apyykone          #+#    #+#             */
-/*   Updated: 2024/03/28 12:00:58 by apyykone         ###   ########.fr       */
+/*   Updated: 2024/03/28 12:26:31 by apyykone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 /** 
-* use of friend function operator << to overload the << operator
-* needs to be declared out of the class cos it's not a member function
-* and uses the Fixed class as a parameter
+* use of different operator overloads
 */
 
-int main( void )
-{
+int main( void ) {
     Fixed a;
-    Fixed const b( 10 );
-    Fixed const c( 42.42f );
-    Fixed const d( b );
-    a = Fixed( 1234.4321f );
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
