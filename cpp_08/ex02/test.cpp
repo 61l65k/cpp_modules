06af1f9bd02ee55806f2a3b459701e0cfb433bd0
alex <alexxpyykonen@gmail.com>
2024-06-26 00:28:16 +0300

    cpp08 add the last rough ex02

---
+++ cpp_08/ex02/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::deque<int> myVector;
+    
+    // Add some elements to the vector
+    for (int i = 0; i < 35; ++i) {
+        myVector.push_back(i);
+        std::cout << "Size: " << myVector.size() << ", Capacity: " << myVector.size() << std::endl;
+    }
+
+    return 0;
+}
