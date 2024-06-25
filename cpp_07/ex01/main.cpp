36f2df9b0d749ad3ccd88c19179be34d085f5eb6
alex <alexxpyykonen@gmail.com>
2024-06-25 16:30:14 +0300

    better tests for cpp07 ex01

--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -2,54 +2,44 @@
 #include <string>
 #include "iter.hpp"
 
-// Function to print an integer (non-template)
-void printInt(const int &n) {
+template <typename T>
+void print(const T &n)
+{
     std::cout << n << " ";
 }
 
-// Function to print a string (non-template)
-void printString(const std::string &s) {
-    std::cout << s << " ";
-}
-
-// Function template to increment a value (can be used as the third parameter)
 template<typename T>
-void increment(T &n) {
+void increment(T &n)
+{
     ++n;
 }
 
 int main()
 {
-    // Test case 1: Printing integer elements
+    // Integer array
     int intArray[] = {1, 2, 3, 4, 5};
     std::size_t intLen = sizeof(intArray) / sizeof(intArray[0]);
 
-    std::cout << "Integer array: ";
-    iter(intArray, intLen, printInt);
-    std::cout << std::endl;
-
-    // Test case 2: Printing string elements
+    // String array
     std::string strArray[] = {"Hello", "world", "!"};
     std::size_t strLen = sizeof(strArray) / sizeof(strArray[0]);
 
-    std::cout << "String array: ";
-    iter(strArray, strLen, printString);
+    std::cout << "Integer array: ";
+    iter(intArray, intLen, print<int>);
     std::cout << std::endl;
 
-    // Test case 3: Modifying integer elements using a function template
-    std::cout << "Original integer array: ";
-    iter(intArray, intLen, printInt);
+    std::cout << "String array: ";
+    iter(strArray, strLen, print<std::string>);
     std::cout << std::endl;
 
     iter(intArray, intLen, increment<int>);
 
     std::cout << "Modified integer array: ";
-    iter(intArray, intLen, printInt);
+    iter(intArray, intLen, print<int>);
     std::cout << std::endl;
 
-    // Test case 4: Modifying string elements using a function template
     std::cout << "Original string array: ";
-    iter(strArray, strLen, printString);
+    iter(strArray, strLen, print<std::string>);
     std::cout << std::endl;
 
     return 0;
