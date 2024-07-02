54808c2e17cf0bc3cb3319db65c090d7a89b9d6f
alex <alexxpyykonen@gmail.com>
2024-07-02 07:21:53 +0300

    + Add the btc program rough version + Add all exercise files

---
+++ cpp_09/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+
+	if (ac != 2)
+	{
+		std::cout << "Error: could not open file" << std::endl;
+		return (1);
+	}
+
+	BitcoinExchange exchange;
+
+	exchange.run(av[1]);
+}
