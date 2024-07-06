013c38c9da52e166ab4cf16c5632921bb940ee29
alex <alexxpyykonen@gmail.com>
2024-07-06 17:50:29 +0300

    cpp09 false the firsdt line in else also

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -77,6 +77,7 @@ void BitcoinExchange::startExchange(std::string &path)
             firstLine = false;
         else
         {
+            firstLine = false;
             if (line.find("|") == line.npos || line.empty() || line.find("|") != 11)
             {
                 std::cout << "Error: bad input => " << line << std::endl;
